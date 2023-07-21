28229eed0cfc4da702dcc0802dfb49d6d0de1301
Teodor Koynov <teodor.koynov2005@gmail.com>
2023-07-22 00:43:02 +0300

    dashboard setup

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -36,7 +36,7 @@ export const StoreModal: React.FC<StoreModalProps> = () => {
 
       const response = await axios.post('/api/stores', values);
 
-      toast.success('Store created.');
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error('Something went wrong.');
     } finally {
